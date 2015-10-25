6ec32566c665f31a485013859e8885052014c648
toneeraj <toneeraj@gmail.com>
2015-10-25 08:30:13 -0400

    Update EarthquakeMarker.java

--- src/module5/EarthquakeMarker.java
+++ src/module5/EarthquakeMarker.java
@@ -94,6 +94,8 @@ public abstract class EarthquakeMarker extends CommonMarker
 	public void showTitle(PGraphics pg, float x, float y)
 	{
 		// TODO: Implement this method
+		pg.fill(0, 0, 0);
+		pg.text(getTitle(), x, y);
 		
 	}
 
